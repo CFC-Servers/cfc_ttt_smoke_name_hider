eb2fb3d4fa70136159b77299d027b23134317199
Pierce <pierce@vulpes.dev>
2021-12-11 22:50:21 -0500

    disable radio commands through smoke

--- lua/autorun/client/cfc_hide_smoke_names.lua
+++ lua/autorun/client/cfc_hide_smoke_names.lua
@@ -1,13 +1,13 @@
 local function closestPoint(point, lineStart, lineEnd)
-	local s = lineEnd - lineStart
-	local w = point - lineStart
-	local ps = w:Dot(s)
-	if ps <= 0 then return lineStart end
+    local s = lineEnd - lineStart
+    local w = point - lineStart
+    local ps = w:Dot(s)
+    if ps <= 0 then return lineStart end
         
     local l2 = s:Dot(s)
     if ps > l2 then
-    	return lineEnd
-	end
+        return lineEnd
+    end
     
     return lineStart + ps/l2 * s
     
@@ -17,34 +17,67 @@ local radiusMultiplier = 6
 local smokeLifetime = 20
 local smokes = {}
 
+function LineIntersectsWithSmoke(startPos, endPos) 
+    for i, smoke in pairs(smokes) do
+        local radius = smoke.radius
+        local smokeOrigin = smoke.origin
+
+        local closest = closestPoint(smokeOrigin, startPos, endPos)
+        
+        local isInside = closest:Distance(smokeOrigin) < radius
+
+        if os.clock() > smoke.expiresAt then
+            table.remove(smokes, i)
+        end
+
+        if isInside then return true end	
+    end
+
+    return false
+end
+
 hook.Add("HUDDrawTargetID", "CFC_SmokeNameHider_HideTargetID", function()
-	for i, smoke in pairs(smokes) do
-		
-		local radius = smoke.radius
-		local smokeOrigin = smoke.origin
-
-		local trace = LocalPlayer():GetEyeTrace()
-		local closest = closestPoint(smokeOrigin, trace.StartPos, trace.HitPos)
-		
-		local isInside = closest:Distance(smokeOrigin) < radius
-			
-		if os.clock() > smoke.expiresAt then
-			table.remove(smokes, i)
-		end
-		
-
-		if isInside then return false end	
-	end
+    local trace = LocalPlayer():GetEyeTrace()
+    if LineIntersectsWithSmoke(trace.StartPos, trace.HitPos) then
+        return false
+    end
 end)
+
 hook.Add("EntityRemoved", "CFC_SmokeNameHider_TrackSmoke", function(ent)
-	if ent:GetClass() == "ttt_smokegrenade_proj" then
-		table.insert(smokes, {
-			radius = ent:GetRadius() * radiusMultiplier,
-			origin = ent:GetPos(),
-			expiresAt = os.clock() + smokeLifetime
-		})
-	end
+    if ent:GetClass() == "ttt_smokegrenade_proj" then
+        table.insert(smokes, {
+            radius = ent:GetRadius() * radiusMultiplier,
+            origin = ent:GetPos(),
+            expiresAt = os.clock() + smokeLifetime
+        })
+    end
 end)
 
+function RADIO:GetTargetType()
+    if not IsValid(LocalPlayer()) then return end
+    local trace = LocalPlayer():GetEyeTrace(MASK_SHOT)
+
+    if not trace or (not trace.Hit) or (not IsValid(trace.Entity)) then return end
+    if LineIntersectsWithSmoke(trace.StartPos, trace.HitPos) then
+        return "quick_nobody", true
+    end
+
+    local ent = trace.Entity
+
+    if ent:IsPlayer() and ent:IsTerror() then
+        if ent:GetNWBool("disguised", false) then
+            return "quick_disg", true
+        else
+            return ent, false
+        end
+    elseif ent:GetClass() == "prop_ragdoll" and CORPSE.GetPlayerNick(ent, "") != "" then
+
+        if DetectiveMode() and not CORPSE.GetFound(ent, false) then
+            return "quick_corpse", isInside or true
+        else
+            return ent, false
+        end
+    end
+end
 
 
